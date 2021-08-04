bd31710227ca542c0fbba61d61eedbc87fe9ce4e
Zhenzhong Duan <zhenzhong.duan@intel.com>
2021-08-05 01:25:31 +0800

    qemu: Add pic feature capability
    So high level management tools could know if pic knob is supported
    in qemu.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -632,6 +632,7 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     QEMU_CAPS_QUERY_DIRTY_RATE, /* accepts query-dirty-rate */
     QEMU_CAPS_RBD_ENCRYPTION, /* Ceph RBD encryption support */
     QEMU_CAPS_TDX_GUEST, /* -object tdx-guest,... */
+    QEMU_CAPS_MACHINE_PIC, /* -machine pic */
 
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;
